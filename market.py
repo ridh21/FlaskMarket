9888b43bf2dc3ba1dba17921babae1b6f999b221
Ridham Patel <127187827+ridh21@users.noreply.github.com>
2024-06-03 22:48:57 +0530

    updated ui

--- market.py
+++ market.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/market")
+def market_page():
+    items = [
+        {'id' : 1, 'name' : 'Phone', 'barcode' : '897654332457', 'price' : 500},
+        {'id' : 2, 'name' : 'Laptop', 'barcode' : '897654332678', 'price' : 900},
+        {'id' : 3, 'name' : 'Keyboard', 'barcode' : '897654332789', 'price' : 150}
+    ]
+    return render_template("market.html", items=items)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True)
+
